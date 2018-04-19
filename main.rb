3aec7554fe6493dcea5166638f195a4c0e10d3c2
projectormato <k.keita811.anuy@gmail.com>
2018-04-19 11:16:48 +0900

    add views dir and add index.erb(top page) & lottery.erb(take lottery page). and modify main.rb(create number and lottery method)

--- main.rb
+++ main.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
 
 get '/' do
-  'Hello, World!'
+  erb:index
+end
+
+post '/number' do
+  @team_number = params[:team_number]
+  erb:index
+end
+
+post '/lottery' do
+  @team_number = params[:team_number].to_i
+  @readers = []
+  @team_number.times { |i|
+    @readers[i] = params[:reader][i]
+  }
+  @sum = params[:sum]
+  @lottery = []
+  for i in 0..3
+    tmp = []
+    params[:grade][i].to_i.times { |j|
+      tmp.push(j%@team_number+1)
+    }
+    @lottery.push(tmp)
+  end
+  erb:lottery
 end
